e2fa71d47701922311346d2c45b739650289ff08
Sasa <sasaestudo2018@gmail.com>
2023-06-12 23:01:48 -0300

    criadotestee2ecypress

---
+++ test/e2e/teste.spec.js
@@ -0,0 +1,15 @@
+describe('',()=>{
+    it('',()=>{
+    })
+})
+
+describe('Testando calculadora',()=>{
+    it('Soma de números',()=>{
+    cy.visit('/')
+    cy.get('[value="7"]').click()
+    cy.get('[value="+"]').click()
+    cy.get('[value="6"]').click()
+    cy.get('[value="="]').click()
+    cy.get('#tela').should('have.text','13')
+    })
+})
